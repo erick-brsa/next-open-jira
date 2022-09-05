fc75a5dd66b32d95867125b3aa20ac97489d5254
Erick Briones <erickalan518@gmail.com>
2022-09-04 23:59:35 -0500

    Configuring Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme, Theme } from '@mui/material';
+import { CssBaseline } from '@mui/material';
+
+const theme: Theme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
